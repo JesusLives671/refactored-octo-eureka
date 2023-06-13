23ed90b3d539d08ef0f4868f6e647ba31492be52
jnewby671 <76269628+jnewby671@users.noreply.github.com>
2023-06-13 10:28:10 -0700

    Initialize fix-sln-issue

---
+++ BTCPayServer.Tests/SeleniumTester.cs
@@ -0,0 +1,431 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using BTCPayServer.Abstractions.Models;
+using BTCPayServer.Lightning;
+using BTCPayServer.Lightning.CLightning;
+using BTCPayServer.Services;
+using BTCPayServer.Tests.Logging;
+using BTCPayServer.Views.Manage;
+using BTCPayServer.Views.Server;
+using BTCPayServer.Views.Stores;
+using BTCPayServer.Views.Wallets;
+using Microsoft.Extensions.Configuration;
+using NBitcoin;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace BTCPayServer.Tests
+{
+    public class SeleniumTester : IDisposable
+    {
+        public IWebDriver Driver { get; set; }
+        public ServerTester Server { get; set; }
+        public WalletId WalletId { get; set; }
+
+        public string StoreId { get; set; }
+
+        public static SeleniumTester Create([CallerMemberNameAttribute] string scope = null, bool newDb = false) =>
+            new SeleniumTester { Server = ServerTester.Create(scope, newDb) };
+
+
+        public async Task StartAsync()
+        {
+            await Server.StartAsync();
+
+            var windowSize = (Width: 1200, Height: 1000);
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets("AB0AC1DD-9D26-485B-9416-56A33F268117");
+            var config = builder.Build();
+
+            // Run `dotnet user-secrets set RunSeleniumInBrowser true` to run tests in browser
+            var runInBrowser = config["RunSeleniumInBrowser"] == "true";
+            // Reset this using `dotnet user-secrets remove RunSeleniumInBrowser`
+
+
+            var chromeDriverPath = config["ChromeDriverDirectory"] ??
+                                    (Server.PayTester.InContainer ? "/usr/bin" : Directory.GetCurrentDirectory());
+
+            var options = new ChromeOptions();
+            if (Server.PayTester.InContainer)
+            {
+                // this must be first option https://stackoverflow.com/questions/53073411/selenium-webdriverexceptionchrome-failed-to-start-crashed-as-google-chrome-is#comment102570662_53073789
+                options.AddArgument("no-sandbox");
+            }
+            if (!runInBrowser)
+            {
+                options.AddArguments("headless");
+            }
+            options.AddArguments($"window-size={windowSize.Width}x{windowSize.Height}");
+            options.AddArgument("shm-size=2g");
+            Driver = new ChromeDriver(chromeDriverPath, options);
+
+            if (runInBrowser)
+            {
+                // ensure maximized window size
+                Driver.Manage().Window.Maximize();
+            }
+
+            Logs.Tester.LogInformation($"Selenium: Using {Driver.GetType()}");
+            Logs.Tester.LogInformation($"Selenium: Browsing to {Server.PayTester.ServerUri}");
+            Logs.Tester.LogInformation($"Selenium: Resolution {Driver.Manage().Window.Size}");
+            Driver.Manage().Timeouts().ImplicitWait = ImplicitWait;
+            GoToRegister();
+            Driver.AssertNoError();
+        }
+
+        internal IWebElement FindAlertMessage(StatusMessageModel.StatusSeverity severity = StatusMessageModel.StatusSeverity.Success)
+        {
+            var el = Driver.FindElements(By.ClassName($"alert-{StatusMessageModel.ToString(severity)}")).FirstOrDefault(e => e.Displayed);
+            if (el is null)
+                throw new NoSuchElementException($"Unable to find alert-{StatusMessageModel.ToString(severity)}");
+            return el;
+        }
+
+        public static readonly TimeSpan ImplicitWait = TimeSpan.FromSeconds(5);
+        public string Link(string relativeLink)
+        {
+            return Server.PayTester.ServerUri.AbsoluteUri.WithoutEndingSlash() + relativeLink.WithStartingSlash();
+        }
+
+        public void GoToRegister()
+        {
+            Driver.Navigate().GoToUrl(Link("/register"));
+        }
+
+        public string RegisterNewUser(bool isAdmin = false)
+        {
+            var usr = RandomUtils.GetUInt256().ToString().Substring(64 - 20) + "@a.com";
+            Logs.Tester.LogInformation($"User: {usr} with password 123456");
+            Driver.FindElement(By.Id("Email")).SendKeys(usr);
+            Driver.FindElement(By.Id("Password")).SendKeys("123456");
+            Driver.FindElement(By.Id("ConfirmPassword")).SendKeys("123456");
+            if (isAdmin)
+                Driver.FindElement(By.Id("IsAdmin")).Click();
+            Driver.FindElement(By.Id("RegisterButton")).Click();
+            Driver.AssertNoError();
+            return usr;
+        }
+
+        public (string storeName, string storeId) CreateNewStore()
+        {
+            Driver.FindElement(By.Id("Stores")).Click();
+            Driver.FindElement(By.Id("CreateStore")).Click();
+            var name = "Store" + RandomUtils.GetUInt64();
+            Driver.FindElement(By.Id("Name")).SendKeys(name);
+            Driver.FindElement(By.Id("Create")).Click();
+            StoreId = Driver.FindElement(By.Id("Id")).GetAttribute("value");
+            return (name, StoreId);
+        }
+
+        public Mnemonic GenerateWallet(string cryptoCode = "BTC", string seed = "", bool importkeys = false, bool privkeys = false, ScriptPubKeyType format = ScriptPubKeyType.Segwit)
+        {
+            Driver.FindElement(By.Id($"Modify{cryptoCode}")).Click();
+            // Modify case
+            if (Driver.PageSource.Contains("id=\"change-wallet-link\""))
+            {
+                Driver.FindElement(By.Id("change-wallet-link")).Click();
+            }
+
+            if (string.IsNullOrEmpty(seed))
+            {
+                var option = privkeys ? "hotwallet" : "watchonly";
+                Logs.Tester.LogInformation($"Generating new seed ({option})");
+                Driver.FindElement(By.Id("generate-wallet-link")).Click();
+                Driver.FindElement(By.Id($"generate-{option}-link")).Click();
+            }
+            else
+            {
+                Logs.Tester.LogInformation("Progressing with existing seed");
+                Driver.FindElement(By.Id("import-wallet-options-link")).Click();
+                Driver.FindElement(By.Id("import-seed-link")).Click();
+                Driver.FindElement(By.Id("ExistingMnemonic")).SendKeys(seed);
+                SetCheckbox(Driver.FindElement(By.Id("SavePrivateKeys")), privkeys);
+            }
+
+            Driver.FindElement(By.Id("ScriptPubKeyType")).Click();
+            Driver.FindElement(By.CssSelector($"#ScriptPubKeyType option[value={format}]")).Click();
+            Driver.FindElement(By.Id("advanced-settings-button")).Click();
+            SetCheckbox(Driver.FindElement(By.Id("ImportKeysToRPC")), importkeys);
+            Driver.FindElement(By.Id("advanced-settings-button")).Click(); // close settings again , otherwise the button might not be clickable for Selenium
+
+            Logs.Tester.LogInformation("Trying to click Continue button");
+            Driver.FindElement(By.Id("Continue")).Click();
+            // Seed backup page
+            FindAlertMessage();
+            if (string.IsNullOrEmpty(seed))
+            {
+                seed = Driver.FindElements(By.Id("recovery-phrase")).First().GetAttribute("data-mnemonic");
+            }
+
+            // Confirm seed backup
+            Driver.FindElement(By.Id("confirm")).Click();
+            Driver.FindElement(By.Id("submit")).Click();
+
+            WalletId = new WalletId(StoreId, cryptoCode);
+            return new Mnemonic(seed);
+        }
+
+        public void AddDerivationScheme(string cryptoCode = "BTC", string derivationScheme = "xpub661MyMwAqRbcGABgHMUXDzPzH1tU7eZaAaJQXhDXsSxsqyQzQeU6kznNfSuAyqAK9UaWSaZaMFdNiY5BCF4zBPAzSnwfUAwUhwttuAKwfRX-[legacy]")
+        {
+            Driver.FindElement(By.Id($"Modify{cryptoCode}")).Click();
+            Driver.FindElement(By.Id("import-wallet-options-link")).Click();
+            Driver.FindElement(By.Id("import-xpub-link")).Click();
+            Driver.FindElement(By.Id("DerivationScheme")).SendKeys(derivationScheme);
+            Driver.FindElement(By.Id("Continue")).Click();
+            Driver.FindElement(By.Id("Confirm")).Click();
+            FindAlertMessage();
+        }
+
+        public void AddLightningNode(string cryptoCode, LightningConnectionType connectionType)
+        {
+            string connectionString;
+            if (connectionType == LightningConnectionType.Charge)
+                connectionString = $"type=charge;server={Server.MerchantCharge.Client.Uri.AbsoluteUri};allowinsecure=true";
+            else if (connectionType == LightningConnectionType.CLightning)
+                connectionString = "type=clightning;server=" + ((CLightningClient)Server.MerchantLightningD).Address.AbsoluteUri;
+            else if (connectionType == LightningConnectionType.LndREST)
+                connectionString = $"type=lnd-rest;server={Server.MerchantLnd.Swagger.BaseUrl};allowinsecure=true";
+            else
+                throw new NotSupportedException(connectionType.ToString());
+
+            Driver.FindElement(By.Id($"Modify-Lightning{cryptoCode}")).Click();
+            Driver.FindElement(By.Name($"ConnectionString")).SendKeys(connectionString);
+            Driver.FindElement(By.Id($"save")).Click();
+        }
+
+        public void AddInternalLightningNode(string cryptoCode)
+        {
+            Driver.FindElement(By.Id($"Modify-Lightning{cryptoCode}")).Click();
+            Driver.FindElement(By.Id($"internal-ln-node-setter")).Click();
+            Driver.FindElement(By.Id($"save")).Click();
+        }
+
+        public void ClickOnAllSideMenus()
+        {
+            var links = Driver.FindElements(By.CssSelector(".nav-pills .nav-link")).Select(c => c.GetAttribute("href")).ToList();
+            Driver.AssertNoError();
+            Assert.NotEmpty(links);
+            foreach (var l in links)
+            {
+                Logs.Tester.LogInformation($"Checking no error on {l}");
+                Driver.Navigate().GoToUrl(l);
+                Driver.AssertNoError();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                try
+                {
+                    Driver.Quit();
+                }
+                catch
+                {
+                    // ignored
+                }
+
+                Driver.Dispose();
+            }
+
+            Server?.Dispose();
+        }
+
+        internal void AssertNotFound()
+        {
+            Assert.Contains("404 - Page not found</h1>", Driver.PageSource);
+        }
+
+        public void GoToHome()
+        {
+            Driver.Navigate().GoToUrl(Server.PayTester.ServerUri);
+        }
+
+        public void Logout()
+        {
+            Driver.FindElement(By.Id("Logout")).Click();
+        }
+
+        public void Login(string user, string password)
+        {
+            Driver.FindElement(By.Id("Email")).SendKeys(user);
+            Driver.FindElement(By.Id("Password")).SendKeys(password);
+            Driver.FindElement(By.Id("LoginButton")).Click();
+        }
+
+        public void GoToStores()
+        {
+            Driver.FindElement(By.Id("Stores")).Click();
+        }
+
+        public void GoToStore(string storeId, StoreNavPages storeNavPage = StoreNavPages.Index)
+        {
+            Driver.FindElement(By.Id("Stores")).Click();
+            Driver.FindElement(By.Id($"update-store-{storeId}")).Click();
+
+            if (storeNavPage != StoreNavPages.Index)
+            {
+                Driver.FindElement(By.Id(storeNavPage.ToString())).Click();
+            }
+        }
+
+        public void GoToInvoiceCheckout(string invoiceId)
+        {
+            Driver.FindElement(By.Id("Invoices")).Click();
+            Driver.FindElement(By.Id($"invoice-checkout-{invoiceId}")).Click();
+            CheckForJSErrors();
+        }
+
+
+        public void SetCheckbox(IWebElement element, bool value)
+        {
+            if ((value && !element.Selected) || (!value && element.Selected))
+            {
+                element.Click();
+            }
+
+            if (value != element.Selected)
+            {
+                Logs.Tester.LogInformation("SetCheckbox recursion, trying to click again");
+                SetCheckbox(element, value);
+            }
+        }
+
+        public void SetCheckbox(SeleniumTester s, string checkboxId, bool value)
+        {
+            SetCheckbox(s.Driver.FindElement(By.Id(checkboxId)), value);
+        }
+
+        public void GoToInvoices()
+        {
+            Driver.FindElement(By.Id("Invoices")).Click();
+        }
+
+        public void GoToProfile(ManageNavPages navPages = ManageNavPages.Index)
+        {
+            Driver.FindElement(By.Id("MySettings")).Click();
+            if (navPages != ManageNavPages.Index)
+            {
+                Driver.FindElement(By.Id(navPages.ToString())).Click();
+            }
+        }
+
+        public void GoToLogin()
+        {
+            Driver.Navigate().GoToUrl(new Uri(Server.PayTester.ServerUri, "/login"));
+        }
+
+        public string CreateInvoice(string storeName, decimal amount = 100, string currency = "USD", string refundEmail = "")
+        {
+            GoToInvoices();
+            Driver.FindElement(By.Id("CreateNewInvoice")).Click();
+            Driver.FindElement(By.Id("Amount")).SendKeys(amount.ToString(CultureInfo.InvariantCulture));
+            var currencyEl = Driver.FindElement(By.Id("Currency"));
+            currencyEl.Clear();
+            currencyEl.SendKeys(currency);
+            Driver.FindElement(By.Id("BuyerEmail")).SendKeys(refundEmail);
+            Driver.FindElement(By.Name("StoreId")).SendKeys(storeName);
+            Driver.FindElement(By.Id("Create")).Click();
+
+            FindAlertMessage();
+            var statusElement = Driver.FindElement(By.ClassName("alert-success"));
+            var id = statusElement.Text.Split(" ")[1];
+            return id;
+        }
+
+        public async Task FundStoreWallet(WalletId walletId = null, int coins = 1, decimal denomination = 1m)
+        {
+            walletId ??= WalletId;
+            GoToWallet(walletId, WalletsNavPages.Receive);
+            Driver.FindElement(By.Id("generateButton")).Click();
+            var addressStr = Driver.FindElement(By.Id("address")).GetProperty("value");
+            var address = BitcoinAddress.Create(addressStr, ((BTCPayNetwork)Server.NetworkProvider.GetNetwork(walletId.CryptoCode)).NBitcoinNetwork);
+            for (var i = 0; i < coins; i++)
+            {
+                await Server.ExplorerNode.SendToAddressAsync(address, Money.Coins(denomination));
+            }
+        }
+
+        public void PayInvoice(WalletId walletId, string invoiceId)
+        {
+            GoToInvoiceCheckout(invoiceId);
+            var bip21 = Driver.FindElement(By.ClassName("payment__details__instruction__open-wallet__btn"))
+                .GetAttribute("href");
+            Assert.Contains($"{PayjoinClient.BIP21EndpointKey}", bip21);
+
+            GoToWallet(walletId);
+            Driver.FindElement(By.Id("bip21parse")).Click();
+            Driver.SwitchTo().Alert().SendKeys(bip21);
+            Driver.SwitchTo().Alert().Accept();
+            Driver.FindElement(By.Id("SendMenu")).Click();
+            Driver.FindElement(By.CssSelector("button[value=nbx-seed]")).Click();
+            Driver.FindElement(By.CssSelector("button[value=broadcast]")).Click();
+        }
+
+        private void CheckForJSErrors()
+        {
+            //wait for seleniun update: https://stackoverflow.com/questions/57520296/selenium-webdriver-3-141-0-driver-manage-logs-availablelogtypes-throwing-syste
+            //            var errorStrings = new List<string> 
+            //            { 
+            //                "SyntaxError", 
+            //                "EvalError", 
+            //                "ReferenceError", 
+            //                "RangeError", 
+            //                "TypeError", 
+            //                "URIError" 
+            //            };
+            //
+            //            var jsErrors = Driver.Manage().Logs.GetLog(LogType.Browser).Where(x => errorStrings.Any(e => x.Message.Contains(e)));
+            //
+            //            if (jsErrors.Any())
+            //            {
+            //                Logs.Tester.LogInformation("JavaScript error(s):" + Environment.NewLine + jsErrors.Aggregate("", (s, entry) => s + entry.Message + Environment.NewLine));
+            //            }
+            //            Assert.Empty(jsErrors);
+
+        }
+
+        public void GoToWallet(WalletId walletId = null, WalletsNavPages navPages = WalletsNavPages.Send)
+        {
+            walletId ??= WalletId;
+            Driver.Navigate().GoToUrl(new Uri(Server.PayTester.ServerUri, $"wallets/{walletId}"));
+            if (navPages != WalletsNavPages.Transactions)
+            {
+                Driver.FindElement(By.Id($"Wallet{navPages}")).Click();
+            }
+        }
+
+        public void GoToUrl(string relativeUrl)
+        {
+            Driver.Navigate().GoToUrl(new Uri(Server.PayTester.ServerUri, relativeUrl));
+        }
+        
+        public void GoToServer(ServerNavPages navPages = ServerNavPages.Index)
+        {
+            Driver.FindElement(By.Id("ServerSettings")).Click();
+            if (navPages != ServerNavPages.Index)
+            {
+                Driver.FindElement(By.Id($"Server-{navPages}")).Click();
+            }
+        }
+
+        public void GoToInvoice(string id)
+        {
+            GoToInvoices();
+            foreach (var el in Driver.FindElements(By.ClassName("invoice-details-link")))
+            {
+                if (el.GetAttribute("href").Contains(id, StringComparison.OrdinalIgnoreCase))
+                {
+                    el.Click();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ BTCPayServer/Controllers/HomeController.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BTCPayServer.Abstractions.Constants;
+using BTCPayServer.Data;
+using BTCPayServer.HostedServices;
+using BTCPayServer.Models;
+using BTCPayServer.Models.StoreViewModels;
+using BTCPayServer.Security;
+using BTCPayServer.Services;
+using BTCPayServer.Services.Apps;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using NBitcoin;
+using NBitcoin.Payment;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BTCPayServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CssThemeManager _cachedServerSettings;
+        private readonly IFileProvider _fileProvider;
+
+        public IHttpClientFactory HttpClientFactory { get; }
+        public LanguageService LanguageService { get; }
+        SignInManager<ApplicationUser> SignInManager { get; }
+
+        public HomeController(IHttpClientFactory httpClientFactory,
+                              CssThemeManager cachedServerSettings,
+                              IWebHostEnvironment webHostEnvironment,
+                              LanguageService languageService,
+                              SignInManager<ApplicationUser> signInManager)
+        {
+            HttpClientFactory = httpClientFactory;
+            _cachedServerSettings = cachedServerSettings;
+            LanguageService = languageService;
+            _fileProvider = webHostEnvironment.WebRootFileProvider;
+            SignInManager = signInManager;
+        }
+
+        private async Task<ViewResult> GoToApp(string appId, AppType? appType)
+        {
+            if (appType.HasValue && !string.IsNullOrEmpty(appId))
+            {
+                this.HttpContext.Response.Headers.Remove("Onion-Location");
+                switch (appType.Value)
+                {
+                    case AppType.Crowdfund:
+                        {
+                            var serviceProvider = HttpContext.RequestServices;
+                            var controller = (AppsPublicController)serviceProvider.GetService(typeof(AppsPublicController));
+                            controller.Url = Url;
+                            controller.ControllerContext = ControllerContext;
+                            var res = await controller.ViewCrowdfund(appId, null) as ViewResult;
+                            if (res != null)
+                            {
+                                res.ViewName = $"/Views/AppsPublic/ViewCrowdfund.cshtml";
+                                return res; // return 
+                            }
+
+                            break;
+                        }
+
+                    case AppType.PointOfSale:
+                        {
+                            var serviceProvider = HttpContext.RequestServices;
+                            var controller = (AppsPublicController)serviceProvider.GetService(typeof(AppsPublicController));
+                            controller.Url = Url;
+                            controller.ControllerContext = ControllerContext;
+                            var res = await controller.ViewPointOfSale(appId) as ViewResult;
+                            if (res != null)
+                            {
+                                res.ViewName = $"/Views/AppsPublic/{res.ViewName}.cshtml";
+                                return res; // return 
+                            }
+
+                            break;
+                        }
+                }
+            }
+            return null;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (_cachedServerSettings.FirstRun)
+            {
+                return RedirectToAction(nameof(AccountController.Register), "Account");
+            }
+            var matchedDomainMapping = _cachedServerSettings.DomainToAppMapping.FirstOrDefault(item =>
+                item.Domain.Equals(Request.Host.Host, StringComparison.InvariantCultureIgnoreCase));
+            if (matchedDomainMapping != null)
+            {
+                return await GoToApp(matchedDomainMapping.AppId, matchedDomainMapping.AppType) ?? GoToHome();
+            }
+
+            return await GoToApp(_cachedServerSettings.RootAppId, _cachedServerSettings.RootAppType) ?? GoToHome();
+        }
+
+        private IActionResult GoToHome()
+        {
+            if (SignInManager.IsSignedIn(User))
+                return View("Home");
+            else
+                return RedirectToAction(nameof(AccountController.Login), "Account");
+        }
+
+        [Route("misc/lang")]
+        [Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+        public IActionResult Languages()
+        {
+            return Json(LanguageService.GetLanguages(), new JsonSerializerSettings() { Formatting = Formatting.Indented });
+        }
+
+        [Route("swagger/v1/swagger.json")]
+        [Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie + "," + AuthenticationSchemes.Greenfield)]
+        public async Task<IActionResult> Swagger()
+        {
+            JObject json = new JObject();
+            var directoryContents = _fileProvider.GetDirectoryContents("swagger/v1");
+            foreach (IFileInfo fi in directoryContents)
+            {
+                await using var stream = fi.CreateReadStream();
+                using var reader = new StreamReader(fi.CreateReadStream());
+                json.Merge(JObject.Parse(await reader.ReadToEndAsync()));
+            }
+            var servers = new JArray();
+            servers.Add(new JObject(new JProperty("url", HttpContext.Request.GetAbsoluteRoot())));
+            json["servers"] = servers;
+            return Json(json);
+        }
+
+        [Route("docs")]
+        [Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+        public IActionResult SwaggerDocs()
+        {
+            return View();
+        }
+
+        [Route("recovery-seed-backup")]
+        [Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie)]
+        public IActionResult RecoverySeedBackup(RecoverySeedBackupViewModel vm)
+        {
+            return View("RecoverySeedBackup", vm);
+        }
+
+        [HttpPost]
+        [Route("postredirect-callback-test")]
+        public ActionResult PostRedirectCallbackTestpage(IFormCollection data)
+        {
+            var list = data.Keys.Aggregate(new Dictionary<string, string>(), (res, key) =>
+            {
+                res.Add(key, data[key]);
+                return res;
+            });
+            return Json(list);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
