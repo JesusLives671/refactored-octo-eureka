98e77d44c65aca7523858c16c701c8ea157815f9
jnewby671 <76269628+jnewby671@users.noreply.github.com>
2023-06-13 10:27:04 -0700

    Initialize migration-fix

---
+++ BTCPayServer.Common/Altcoins/Monero/BTCPayNetworkProvider.Monero.cs
@@ -0,0 +1,26 @@
+using NBitcoin;
+
+namespace BTCPayServer
+{
+    public partial class BTCPayNetworkProvider
+    {
+        public void InitMonero()
+        {
+            Add(new MoneroLikeSpecificBtcPayNetwork()
+            {
+                CryptoCode = "XMR",
+                DisplayName = "Monero",
+                BlockExplorerLink =
+                    NetworkType == NetworkType.Mainnet
+                        ? "https://www.exploremonero.com/transaction/{0}"
+                        : "https://testnet.xmrchain.net/tx/{0}",
+                DefaultRateRules = new[]
+                {
+                    "XMR_X = XMR_BTC * BTC_X",
+                    "XMR_BTC = kraken(XMR_BTC)"
+                },
+                CryptoImagePath = "/imlegacy/monero.svg"
+            });
+        }
+    }
+}
